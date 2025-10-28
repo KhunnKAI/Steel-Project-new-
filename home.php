3aacede7c7b1f880ac8e8883e4a64fca2cca228b
KKMM\USER <prawitchaya.kim@gmail.com>
2025-10-28 21:12:54 +0700

    no message

--- home.php
+++ home.php
@@ -455,7 +455,7 @@
         <!-- Video Section -->
         <section class="video-section">
             <div class="youtube-container">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/c8OIlRJweGA?si=TxokhJaYy5tusPtY"
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/iAKN2ngeS9o"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
